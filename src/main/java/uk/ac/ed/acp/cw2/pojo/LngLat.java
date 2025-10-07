0135213348cefa7f1e979c344aa60f141f6a9eeb
franksun-11 <s2564099@ed.ac.uk>
2025-10-07 17:10:52 +0100

    Edit isValid: Make sure input position is within Edinburgh Area

--- src/main/java/uk/ac/ed/acp/cw2/pojo/LngLat.java
+++ src/main/java/uk/ac/ed/acp/cw2/pojo/LngLat.java
@@ -23,12 +23,16 @@ public class LngLat {
     private Double lat;
 
     /**
-     * Verify if latitude and longitude and valid
+     * Verify if latitude and longitude are valid for Edinburgh area
+     * Longitude should be negative (West) and latitude should be positive (North)
      */
     @JsonIgnore
     public boolean isValid() {
         return lng != null && lat != null &&
+                lng >= -180 && lng <= 0 &&    // longitude must be negative (West)
+                lat >= 0 && lat <= 90 &&      // latitude must be positive (North)
                 lng >= -180 && lng <= 180 &&
                 lat >= -90 && lat <= 90;
     }
+
 }
